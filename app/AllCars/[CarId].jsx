fa056ab4d369ffc8970836a6e8e1c1213cb0075b
sajjad <sajjadhasanikakh06@gmail.com>
2024-10-18 11:14:51 +0330

    add

---
+++ app/AllCars/[CarId].jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div>
+        sallam khobi
+    </div>
+  )
+}
+
+export default Details;
--- components/module/Car/Car.jsx
+++ components/module/Car/Car.jsx
@@ -1,22 +1,35 @@
 import carsData from "@/data/DataCars";
 import Image from "next/image";
-
+import Link from "next/link";
+import "./Car.css";
 const Car = () => {
   return (
     <div className="flex justify-center max-w-[900px] m-auto items-center flex-wrap max-[768px]:max-w-full">
       {carsData.map((indexcar) => (
         <div className="items_cars flex flex-col flex-wrap " key={indexcar.id}>
-            {/* img cars */}
-          <div className="img_cars max-[768px]:m-2">
-            <Image src={indexcar.image} width={300} height={300} className="w-[250px] h-[250px] rounded-lg m-5 max-[768px]:w-full max-[768px]:m-auto"/>
+          {/* img cars */}
+          <div className="img_cars max-[768px]:m-2  m-5">
+            <Link href={`/AllCars/${indexcar.id}`}>
+              <Image
+                src={indexcar.image}
+                width={300}
+                height={300}
+                className="w-[250px] h-[250px] rounded-lg  max-[768px]:w-full max-[768px]:m-auto"
+                title="Details"
+              />
+            </Link>
           </div>
           {/* name cars */}
           <div className="name_cars  flex flex-col">
-            <h1 className="text-center font-bold text-[20px]">{indexcar.name}</h1>
+            <h1 className="text-center font-bold text-[20px]">
+              {indexcar.name}
+            </h1>
           </div>
           {/* location and prise */}
           <div className="location flex justify-around items-center mt-5 mb-5">
-            <h1 className="bg-green-200 rounded-lg px-1 py-1">{indexcar.price}</h1>
+            <h1 className="bg-green-200 rounded-lg px-1 py-1">
+              {indexcar.price}
+            </h1>
             <h1>{indexcar.location}</h1>
           </div>
         </div>
